e7de9dc9312bffb3a8156cf3859fd3c636646c6f
nonu007 <nonu22799@gmail.com>
2026-07-02 11:40:58 +0530

    added pcap reader file (header and cpp ) for reading the pcap file

--- include/pcap_reader.h
+++ include/pcap_reader.h
@@ -4,81 +4,83 @@
 #include <cstdint>
 #include <vector>
 #include <string>
-#include <fstream> // for file handling
+#include <fstream>
 
-namespace packetAnalyzer
+namespace PacketAnalyzer
 {
-
-    // Global pcap header at the very begining  every pcap file.
-
+    // Pcap global header, starting 24 bytes of file
+    // this isi the very beggining of the packet
     struct PcapGlobalHeader
     {
 
-        uint32_t magic_number;  // for swap (for big endian)   , endianness => (matlab data seedha read karna hai ya reverse karke)
-        uint16_t version_major; // PCAP file ka format version (usually 2.4)
-        uint16_t version_minor; // PCAP file ka format version (usually 2.4)
-        int32_t thiszone;       // usulaly 0  (ignore)
-        uint32_t sigfigs;       // timestamps usually 0
-        uint32_t snaplen;       // max length of captured packet
-        uint32_t network;       // data link layer (1 means ethernet )
+        uint32_t magic_number; // 0xa1b2c3d4
+        uint16_t version_major;
+        uint16_t version_minor;
+        int32_t thiszone;
+        uint32_t sigfigs; // timestamps
+        uint32_t snaplen; // max lengeth of captured packet
+        uint32_t network; // data link layer( ETherneet =1, Tls =2, TCP=3,.... etc)
     };
-
-    // PCAP header
-    struct Pcap_packetHeader
+    // Each packet in the file starts from these listed header
+    // pcap header 16 bytes
+    struct PcapPacketHeader
     {
-        uint32_t ts_sec;      // timestamp in seconds
-        uint32_t ts_usec;     // timestamp in micro-seconds
-        uint32_t incl_length; // number of bytes saved in a file
-        uint32_t orig_length; // actual length of packet
+        uint32_t ts_sec;      // timestamps in seconds
+        uint32_t ts_usec;     // timestamps in micro-seconds
+        uint32_t inc_len;  // number of bytes saved in file
+        uint32_t orig_len; // actual length of packet
     };
 
-    // represent a single captured packet
+    // struct of a singlr capture  packet
+
     struct RawPacket
     {
-        Pcap_packetHeader header;
-        std::vector<uint8_t> data; // actual packet bytes data
+        PcapPacketHeader header;
+        std::vector<uint8_t> data;
     };
-
-    // class to read pcap files
     class PcapReader
     {
-    public:
-        PcapReader() = default;
-        ~PcapReader();
-
-        // open th file for reading
-        bool open(const std::string &filename);
-
-        // close the file after reading
-        void close();
-
-        // read the next packet , if no more packet found , return false
-        bool readNextPacket(RawPacket &packet);
-
-        // get the globaheader of the file
-        const PcapGlobalHeader &getGlobalHeader() const
-        {
-            return global_header_;
-        }
-
-        // check is the file open
-        bool isOpen() const { return file_.is_open(); }
-
-        // check if we need to swap the byte order
-        bool needsByteSwap() const
-        {
-            return needs_byte_swap_;
-        }
 
     private:
         std::ifstream file_;
         PcapGlobalHeader global_header_;
         bool needs_byte_swap_ = false;
 
-        // helper to say bytes if needed
-        uint16_t maybeswap16(uint16_t value);
-        uint32_t maybeswap32(uint32_t value);
-    };
-} // namespace PacketAnalyzer
+        // helper if need to swap byte
+        uint16_t maybeSwap16(uint16_t value);
+        uint32_t maybeSwap32(uint32_t value);
 
-#endif // PCAP_READER_H
+    public:
+        PcapReader() = default;
+        ~PcapReader();
+
+        // open the fie for reading the packet
+        bool open(const std::string &filename);
+
+        // close the file
+        void close();
+
+        // read the next packet , retrn false if no more packets recieved
+        bool ReadNextPacket(RawPacket &packet);
+
+        // if recieved the packet , get the global header info
+        const PcapGlobalHeader &getGlobalHeader() const
+        {
+            return global_header_;
+        }
+
+        // checking file is open
+        bool isOpen() const
+        {
+            return file_.is_open();
+        }
+
+        // check if we need to swap byte
+        bool needsByteSwap() const {
+             return needs_byte_swap_;
+        }
+    };
+
+} // end of namespace ppacketAnalyzer
+
+#endif // end of pcap readere
--- src/pcap_Reader.cpp
+++ src/pcap_Reader.cpp
@@ -2,37 +2,41 @@
 #include <cstring>
 #include <iostream>
 
-namespace packetAnalyzer
+namespace PacketAnalyzer // same namespace as in pcapreader header file
 {
-   // Magic numbers for PCAP files
-   constexpr uint32_t PCAP_MAGIC_NATIVE = 0X1a2b3c4d;  // native byte order
-   constexpr uint32_t PCAP_MAGIC_SWAPPED = 0X4d3c2b1a; // swapped byte order
 
-   PcapReader::~PcapReader()
+   // magic numbers for pcap file
+   // constexpr :- "Ye value compile time pe hi fix hai, kabhi nahi badlegi, memory waste mat karo"
+   constexpr uint32_t PCAP_MAGIC_NATIVE = 0xa1b2c3d4;  // native byte order (withour swap)
+   constexpr uint32_t PCAP_MAGIC_SWAPPED = 0xd4c3b2a1; // why swapped , bcoz Intel processor bytes ulte store karta hai
+
+   PcapReader ::~PcapReader()
    {
       close();
    }
-   bool PcapReader::open(const std::string &filename)
+   // open the file and getting the header and adding it to create rawpacket
+   bool PcapReader ::open(const std::string &filename)
    {
-      // close the file already opened
+      // close file if previosuly opened
       close();
-      // open in binary mode , crucial for reading raw bytes
+
+      // open file in binary mode
       file_.open(filename, std::ios::binary);
       if (!file_.is_open())
       {
-         std::cerr << "Error: Could not open file " << filename << std::endl;
-         return false;
+         std::cerr << " Error: Could not open file " << filename << std::endl;
       }
-      // read the global header (first 24 bytes of file )
+
+      // read the globla header from the file
       file_.read(reinterpret_cast<char *>(&global_header_), sizeof(PcapGlobalHeader));
+      // if file not good
       if (!file_.good())
       {
-         std::cerr << "Error: Could not read PCAP global header from the file " << filename << std::endl;
+         std::cerr << " Error : Sorry Couldn't read Pcap global header  " << std::endl;
          close();
          return false;
       }
-
-      // check the magic number is determine byte order
+      // check the magic number to determine the byte order
       if (global_header_.magic_number == PCAP_MAGIC_NATIVE)
       {
          needs_byte_swap_ = false;
@@ -40,32 +44,31 @@ namespace packetAnalyzer
       else if (global_header_.magic_number == PCAP_MAGIC_SWAPPED)
       {
          needs_byte_swap_ = true;
-
-         // swap the global header fields to correct byte order
-         global_header_.version_major = maybeswap16(global_header_.version_major);
-         global_header_.version_minor = maybeswap16(global_header_.version_minor);
-         // global_header_.thiszone = maybeswap32(global_header_.thiszone);
-         // global_header_.sigfigs = maybeswap32(global_header_.sigfigs);
-         global_header_.snaplen = maybeswap32(global_header_.snaplen);
-         global_header_.network = maybeswap32(global_header_.network);
+         // swap the header fields to the correct byte order
+         global_header_.version_major = maybeSwap16(global_header_.version_major);
+         global_header_.version_minor = maybeSwap16(global_header_.version_minor);
+         global_header_.snaplen = maybeSwap32(global_header_.snaplen);
+         global_header_.network = maybeSwap32(global_header_.network);
       }
       else
       {
-         std::cerr << "Error: Invalid PCAP file " << filename << std::endl;
+         std::cerr << "Error : Invalid PCAP magic number : OK"
+                   << std::hex << global_header_.magic_number << std::dec << std::endl;
          close();
          return false;
       }
 
-      std::cout << "Successfully opened PCAP file " << filename << std::endl;
-      std::cout << "Version " << global_header_.version_major << "." << global_header_.version_minor << std::endl;
-      std::cout << "Snaplen " << global_header_.snaplen << "bytes" << std::endl;
-      std::cout << "Link-layer type " << global_header_.network << (global_header_.network == 1 ? "(Ethernet)" : "") << std::endl;
+      std::cout << "Opened PCAP file" << filename << std::endl;
+      std::cout << "Version " << global_header_.version_major
+                << "." << global_header_.version_minor << std::endl;
+      std::cout << "Snap len" << global_header_.snaplen << " bytes" << std::endl;
+      std::cout << "Link type " << global_header_.network << (global_header_.network == 1 ? " (Ethernet) " : "") << std::endl;
 
       return true;
    }
 
-   // checking file is open if it is the close it and no need to swap
-   void PcapReader::close()
+   // after create raw packet , close the file
+   void PcapReader ::close()
    {
       if (file_.is_open())
       {
@@ -74,64 +77,60 @@ namespace packetAnalyzer
       needs_byte_swap_ = false;
    }
 
-   bool PcapReader::readNextPacket(RawPacket &packet)
+   bool PcapReader ::ReadNextPacket(RawPacket &packet)
    {
       if (!file_.is_open())
       {
-         std::cerr << "Error: No file is open for reading packets." << std::endl;
          return false;
       }
 
-      // read the packet header (16 bytes)
-      Pcap_packetHeader pkt_header;
-      file_.read(reinterpret_cast<char *>(&pkt_header), sizeof(Pcap_packetHeader));
+      // read the header in (16 bytes)
+      file_.read(reinterpret_cast<char *>(&packet.header), sizeof(PcapPacketHeader));
       if (!file_.good())
       {
-         return false; // no more packets to read
-      }
-
-      // swap the packet header fields if needed
-      if (needs_byte_swap_)
-      {
-         pkt_header.ts_sec = maybeswap32(pkt_header.ts_sec);
-         pkt_header.ts_usec = maybeswap32(pkt_header.ts_usec);
-         pkt_header.incl_length = maybeswap32(pkt_header.incl_length);
-         pkt_header.orig_length = maybeswap32(pkt_header.orig_length);
-      }
-
-      // read the packet data based on incl_length
-      packet.header = pkt_header;
-      packet.data.resize(pkt_header.incl_length);
-      file_.read(reinterpret_cast<char *>(packet.data.data()), pkt_header.incl_length);
-      if (!file_.good())
-      {
-         std::cerr << "Error: Could not read complete packet data." << std::endl;
          return false;
       }
-
-      return true; // successfully read a packet
-   }
-   // may be swap for 16 bit 
-   uint16_t PcapReader::maybeswap16(uint16_t value)
-   {
-      if (needs_byte_swap_)
-      {
-         return (value >> 8) | (value << 8);
+      // swap bytes if needed
+      if(needs_byte_swap_){
+         packet.header.ts_sec = maybeSwap32(packet.header.ts_sec);
+         packet.header.ts_usec = maybeSwap32(packet.header.ts_usec);
+         packet.header.inc_len = maybeSwap32(packet.header.inc_len);
+         packet.header.orig_len = maybeSwap32(packet.header.orig_len);
       }
-      return value;
-   }
 
-      // may be swap for 32 bit
-   uint32_t PcapReader::maybeswap32(uint32_t value)
-   {
-      if (needs_byte_swap_)
-      {
-         return ((value >> 24) & 0x000000FF) |
-                ((value >> 8) & 0x0000FF00) |
-                ((value << 8) & 0x00FF0000) |
-                ((value << 24) & 0xFF000000);
+      // sanity check on packet length
+      if(packet.header.inc_len > global_header_.snaplen 
+         ||packet.header.inc_len > 65535){
+         std::cerr << " Error : invalid packet length "
+         <<packet.header.inc_len <<std::endl;
       }
-      return value;
+     
+      // read the packet data 
+      packet.data.resize(packet.header.inc_len);
+      file_.read(reinterpret_cast<char*> (packet.data.data()),
+      packet.header.inc_len);
+      if(!file_.good()){
+         std::cerr << "Error : Could not read packet data "<<std::endl;
+         return false;
+      }
+      return true;
+
    }
 
+   // swap into 16 bytes
+   uint16_t PcapReader ::maybeSwap16(uint16_t value) {
+      if(!needs_byte_swap_){
+         return value;
+      } return ((value & 0x00FF) <<8 ) | ((value & 0xFF00) >> 8);
+   }
+
+   // swap into 16 bytes
+   uint32_t PcapReader ::maybeSwap32(uint32_t value) {
+      if(!needs_byte_swap_){
+         return value;
+      } return ((value & 0x000000FF) << 24) |
+                ((value & 0x0000FF00) << 8) |
+                ((value & 0x00FF0000) >> 8) |
+                ((value & 0xFF000000) >> 24);
+   }
 } // end of namespace PacketAnalyzer
